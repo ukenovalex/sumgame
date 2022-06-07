b13e2e913a55da4e46794c4089998c2f1dbe431a
ukenov <ykenovalex97@gmail.com>
2022-06-08 01:31:40 +0500

    vm to game fragment

--- app/src/main/java/ru/ukenov/sumgame/presentation/GameFragment.kt
+++ app/src/main/java/ru/ukenov/sumgame/presentation/GameFragment.kt
@@ -1,10 +1,14 @@
 package ru.ukenov.sumgame.presentation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import ru.ukenov.sumgame.R
 import ru.ukenov.sumgame.databinding.FragmentGameBinding
 import ru.ukenov.sumgame.domain.entity.GameResult
@@ -12,6 +16,23 @@ import ru.ukenov.sumgame.domain.entity.GameSettings
 import ru.ukenov.sumgame.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[GameViewModel::class.java]
+    }
+    private val textViewOptions by lazy {
+        mutableListOf<TextView>().apply {
+            add(binding.tvOption1)
+            add(binding.tvOption2)
+            add(binding.tvOption3)
+            add(binding.tvOption4)
+            add(binding.tvOption5)
+            add(binding.tvOption6)
+        }
+    }
 
     private lateinit var level: Level
 
@@ -34,22 +55,66 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvOption2.setOnClickListener { onFinishGame() }
+        observeViewModel()
+        setClickListenersToOptions()
+        viewModel.startGame(level)
     }
 
-    private fun onFinishGame() {
+    private fun setClickListenersToOptions() {
+        for(tvOption in textViewOptions) {
+            tvOption.setOnClickListener {
+                viewModel.chooseAnswer(tvOption.text.toString().toInt())
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.formattedTime.observe(viewLifecycleOwner) {
+            binding.tvTimer.text = it
+        }
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumber.toString()
+            for (i in 0 until textViewOptions.size) {
+                textViewOptions[i].text = it.options[i].toString()
+            }
+        }
+        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+        viewModel.enoughCountOfRightAnswers.observe(viewLifecycleOwner) {
+
+            binding.tvAnswersProgress.setTextColor(getColorByState(it))
+        }
+        viewModel.progressAnswers.observe(viewLifecycleOwner) {
+            binding.tvAnswersProgress.text = it.toString()
+        }
+        viewModel.enoughPercentOfRightAnswers.observe(viewLifecycleOwner) {
+            val color = getColorByState(it)
+            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
+        }
+        viewModel.minPercent.observe(viewLifecycleOwner) {
+            binding.progressBar.secondaryProgress = it
+        }
+        viewModel.gameResult.observe(viewLifecycleOwner) {
+            onFinishGame(it)
+        }
+    }
+
+    private fun getColorByState(goodState: Boolean): Int {
+        val colorResId = if (goodState) {
+            android.R.color.holo_green_light
+        } else {
+            android.R.color.holo_red_light
+        }
+        return ContextCompat.getColor(requireContext(), colorResId)
+    }
+
+    private fun onFinishGame(gameResult: GameResult) {
         requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(GameResult(
-                winner = true,
-                countOfQuestions = 10,
-                countOfRightAnswers = 10,
-                gameSettings = GameSettings(
-                    maxSumValue = 10,
-                    gameTimeInSeconds = 10,
-                    minPercentOfRightAnswers = 10,
-                    minCountOfRightAnswers = 10
-                )
-            )))
+            .replace(
+                R.id.main_container, GameFinishedFragment.newInstance(gameResult)
+            )
             .addToBackStack(null)
             .commit()
     }
@@ -77,5 +142,4 @@ class GameFragment : Fragment() {
             }
         }
     }
-
 }
--- app/src/main/java/ru/ukenov/sumgame/presentation/GameViewModel.kt
+++ app/src/main/java/ru/ukenov/sumgame/presentation/GameViewModel.kt
@@ -67,6 +67,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         getGameSettings(level)
         startTimer()
         generateQuestion()
+        updateProgress()
     }
 
     fun chooseAnswer(number: Int) {
@@ -89,6 +90,9 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun calculatePercentOfRightAnswers(): Int {
+        if (countOfQuestions == 0) {
+            return 0
+        }
         return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 
